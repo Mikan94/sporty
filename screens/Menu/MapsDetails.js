1d10f0405a5abb0171edb25c0686757d10a75229
Anne <anne@Annes-MBP.fritz.box>
2019-12-22 17:50:43 +0100

    add maps

--- screens/Menu/MapsDetails.js
+++ screens/Menu/MapsDetails.js
@@ -1,16 +1,45 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
+import MapView, { Marker, Polygon } from 'react-native-maps';
 
 export default class MapsDetails extends Component {
+  state={
+    route1: [
+      {name: '1', latitude: 37.8025259, longitude: -122.4351431},
+      {name: '2', latitude: 37.7896386, longitude: -122.421646},
+      {name: '3', latitude: 37.7665248, longitude: -122.4161628},
+      {name: '4', latitude: 37.7734153, longitude: -122.4577787},
+      {name: '5', latitude: 37.7948605, longitude: -122.4596065}
+    ]
+  }
+
   render() {
+
     return (
-      <View style={styles.container}>
-        <Text>MapDetails</Text>
-        <Button
-          title="Start"
-          onPress={() => this.props.navigation.navigate('RunScreen')}
-        />
-      </View>
+      <MapView
+        style={{ flex: 1}}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.035
+        }}>
+
+        <Polygon
+          coordinates={this.state.route1}/>
+
+        <Marker
+          coordinate={{latitude: 37.78825, longitude: -122.4324}}>
+          </Marker>
+          <View>
+          <Text>Kilometer: 17km </Text>
+          <Text>Zeit: 1:35h </Text>
+        </View>
+
+
+        </MapView>
+
+        
     );
   }
 }
